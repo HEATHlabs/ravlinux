f3bbdab03d9e6b871f3545be2445aafd72b98e1c
Joakim Andersson <joakimer@stud.ntnu.no>
2014-03-10 12:23:21 +0100

    added irq macro, fixed compile issues

--- linux-3.12.13/arch/arm/mach-shmac/shmac.c
+++ linux-3.12.13/arch/arm/mach-shmac/shmac.c
@@ -1,5 +1,13 @@
 //#include <kernel.h>
+#include <linux/kernel.h>
+#include <linux/of_platform.h>
 
+#include <asm/mach/arch.h>
+
+#include <linux/of_irq.h>
+#include <linux/irqchip.h>
+#include <linux/clocksource.h>
+/*
 static struct map_desc shmac_io_desc[] __initdata = {
 	{
 		.virtual = (unsigned long) SHMAC_REGS_VIRT_BASE,
@@ -12,12 +20,13 @@ static struct map_desc shmac_io_desc[] __initdata = {
 void __init shmac_map_io(void){
 	iotable_init(shmac_io_desc, ARRAY_SIZE(shmac_io_desc));
 }
-
+*/
+/*
 struct sys_timer shmac_timer = {
 	.init = shmac_timer_init,
 };
-
-static void __init shmac_dt_init(void){
+*/
+static void __init shmac_init(void){
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
 }
 
@@ -27,12 +36,12 @@ static char const *shmac_dt_compat[] = {
 };
 
 DT_MACHINE_START(SHMAC_DT, "SHMAC")
-        .init_irq = ircqchip_init,
-	.map_io = shmac._map_io,
-	.handle_irq = shmac_handle_irq,
-	.init_machine = shmac_machine_init,
-	.time = shmac_timer,
+//.init_irq = ircqchip_init,
+//	.map_io = shmac._map_io,
+//	.handle_irq = shmac_handle_irq,
+	.init_machine = shmac_init,
+//	.time = shmac_timer,
 	.dt_compat = shmac_dt_compat,
-	.restart = shmac_restart,
+//	.restart = shmac_restart,
 MACHINE_END
 
