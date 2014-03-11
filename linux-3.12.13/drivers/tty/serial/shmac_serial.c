282f42312dd3a9cbb54e2fa0ee1e122066615bc8
Joakim Andersson <joakimer@stud.ntnu.no>
2014-03-11 19:22:36 +0100

    Added skeleton shmac serial console

---
+++ linux-3.12.13/drivers/tty/serial/shmac_serial.c
@@ -0,0 +1,287 @@
+#if defined(CONFIG_SERIAL_SHMAC_UART_CONSOLE) && defined(CONFIG_MAGIC_SYSRQ)
+#define SUPPORT_SYSRQ
+#endif
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/io.h>
+#include <linux/platform_device.h>
+#include <linux/console.h>
+#include <linux/sysrq.h>
+#include <linux/serial_core.h>
+#include <linux/tty_flip.h>
+#include <linux/slab.h>
+#include <linux/clk.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+
+#include <mach/shmac.h>
+
+#define DRIVER_NAME "shmac-uart"
+#define DEV_NAME "ttyshmc"
+
+#define UART_NR 13
+
+#define UARTn_STATUS 0x20
+#define UARTn_STATUS_TXBUSY 0x2
+#define UARTn_TXDATA 0x0
+
+struct shmac_uart_port {
+	struct uart_port port;
+};
+
+/* See Documentation/serial/driver for implementation details */
+
+static unsigned int shmac_uart_tx_empty(struct uart_port *port)
+{
+	return TIOCSER_TEMT;
+}
+
+static void shmac_uart_set_mctrl(struct uart_port *port, unsigned int mctrl)
+{
+	/* do nothing */
+}
+
+static unsigned int shmac_uart_get_mctrl(struct uart_port *port)
+{
+	/* Should indicate signal is permanently active */
+	return TIOCM_CAR | TIOCM_CTS | TIOCM_DSR;
+}
+
+static void shmac_uart_stop_tx(struct uart_port *port)
+{
+	/* Stop transmitting characters */
+}
+
+static void shmac_uart_start_tx(struct uart_port *port)
+{
+	/* Start transmitting characters */
+}
+
+static void shmac_uart_stop_rx(struct uart_port *port)
+{
+	/* Stop receiving characters, the port is in the process of being closed */
+}
+
+static void shmac_uart_enable_ms(struct uart_port *port)
+{
+	/* Enable the modem status interrupts */
+}
+
+static void shmac_uart_break_ctl(struct uart_port *port, int ctl)
+{
+	/* Control the transission of a break signal */
+}
+
+static int shmac_uart_startup(struct uart_port *port)
+{
+	// TODO
+	return 0;
+}
+
+static void shmac_uart_shutdown(struct uart_port *port)
+{
+	// TODO
+}
+
+static void shmac_uart_set_termios(struct uart_port *port, struct ktermios *new, struct ktermios *old)
+{
+	// TODO
+	/* Change the port parameters */
+}
+
+static const char * shmac_uart_type(struct uart_port *port)
+{
+	return "shmac-uart";
+}
+
+static void shmac_uart_release_port(struct uart_port *port)
+{
+	/* Release memory and io requested by port*/
+	// TODO
+}
+
+static int shmac_uart_request_port(struct uart_port *port)
+{
+	/* Request memory and io for port, return EBUSY on failure*/
+	// TODO
+	return 0;
+}
+
+static void shmac_uart_config_port(struct uart_port *port, int type)
+{
+	/* Perform autoconfiguration */
+}
+
+static int shmac_uart_verify_port(struct uart_port *port, struct serial_struct *serinfo )
+{
+	return 0;
+}
+
+static struct uart_ops shmac_uart_port_ops = {
+	.tx_empty = shmac_uart_tx_empty,
+	.set_mctrl = shmac_uart_set_mctrl,
+	.get_mctrl = shmac_uart_get_mctrl,
+	.stop_tx = shmac_uart_stop_tx,
+	.start_tx = shmac_uart_start_tx,
+	.stop_rx = shmac_uart_stop_rx,
+	.enable_ms = shmac_uart_enable_ms,
+	.break_ctl = shmac_uart_break_ctl,
+	.startup = shmac_uart_startup,
+	.shutdown = shmac_uart_shutdown,
+	.set_termios = shmac_uart_set_termios,
+	.type = shmac_uart_type,
+	.release_port = shmac_uart_release_port,
+	.request_port = shmac_uart_request_port,
+	.config_port = shmac_uart_config_port,
+	.verify_port = shmac_uart_verify_port,
+};
+
+static struct shmac_uart_port *shmac_uart_ports[1];
+
+#ifdef CONFIG_SERIAL_SHMAC_UART_CONSOLE
+static void shmac_console_putchar(struct uart_port *port, int ch)
+{
+	struct shmac_uart_port *usp = (struct shmac_uart_port *)port;
+
+	while (readw(usp->port.membase + UARTn_STATUS) & UARTn_STATUS_TXBUSY)
+		;//barrier();
+	writew(ch, usp->port.membase + UARTn_TXDATA);
+}
+
+static void
+shmac_uart_console_write(struct console *co, const char *s, unsigned int count)
+{
+	struct shmac_uart_port *shmac_port = shmac_uart_ports[co->index];
+	uart_console_write(&shmac_port->port, s, count, shmac_console_putchar);
+}
+static struct uart_driver shmac_uart_reg;
+
+static struct console shmac_uart_console = {
+	.name = DEV_NAME,
+	.write = shmac_uart_console_write,
+	.device = uart_console_device,
+	.flags = CON_PRINTBUFFER,
+	.index = -1,
+	.data = &shmac_uart_reg,
+};
+
+#else
+#define shmac_uart_console (*(struct console *)NULL)
+#endif /*ifdef CONFIG_SERIAL_SHMAC_UART_CONSOLE */
+
+static struct uart_driver shmac_uart_reg = {
+	.owner = THIS_MODULE,
+	.driver_name = DRIVER_NAME,
+	.dev_name = DEV_NAME,
+	.nr = ARRAY_SIZE(shmac_uart_ports),
+	.cons = &shmac_uart_console,
+};
+
+static int shmac_uart_probe(struct platform_device *pdev)
+{
+
+	struct shmac_uart_port *shmac_port;
+	//struct vendor_data *vendor = id->data;
+	//void __iomem *base;
+	struct resource *res;
+	int ret;
+
+	shmac_port = devm_kzalloc(&pdev->dev, sizeof(struct shmac_uart_port),
+			   GFP_KERNEL);
+	if (shmac_port == NULL) {
+		dev_dbg(&pdev->dev, "failed to allocate private data\n");
+		return -ENOMEM;
+	}
+	res = platform_get_resource(pdev, IORESOURCE_MEM,0);
+	if(res == NULL){
+		ret = -ENODEV;
+		dev_dbg(&pdev->dev, "failed to get base address\n");
+		kfree(shmac_port);
+		return ret;
+	}
+	
+	ret = platform_get_irq(pdev,1);
+	if(ret <= 0){
+		ret = -ENODEV;
+		dev_dbg(&pdev->dev, "failed to get rx irq\n");
+		kfree(shmac_port);
+		return ret;
+	}
+	shmac_port->port.irq = ret;
+
+	/*i = pl011_probe_dt_alias(i, &dev->dev);
+
+	base = devm_ioremap(&dev->dev, dev->res.start,
+			    resource_size(&dev->res));
+	if (!base) {
+		ret = -ENOMEM;
+		return
+		}*/
+	return 0;
+}
+
+static int shmac_uart_remove(struct platform_device *pdev)
+{
+	struct shmac_uart_port *shmac_port = platform_get_drvdata(pdev);
+	unsigned int line = shmac_port->port.line;
+
+	uart_remove_one_port(&shmac_uart_reg, &shmac_port->port);
+
+	if (line >= 0 && line < ARRAY_SIZE(shmac_uart_ports))
+		shmac_uart_ports[line] = NULL;
+
+	kfree(shmac_port);
+
+	return 0;
+}
+
+static const struct of_device_id shmac_uart_dt_ids[] = {
+	{
+		.compatible = "shmac,shmac-uart",
+	}, {
+		/* sentinel */
+	}
+};
+
+MODULE_DEVICE_TABLE(of, shmac_uart_dt_ids);
+
+static struct platform_driver shmac_uart_driver = {
+	.probe = shmac_uart_probe,
+	.remove = shmac_uart_remove,
+	.driver = {
+		.name = DRIVER_NAME,
+		.owner = THIS_MODULE,
+		.of_match_table = shmac_uart_dt_ids,
+	},
+};
+
+static int __init shmac_serial_init(void)
+{
+	int ret;
+	while(true);
+	printk("shmac serial_console init");
+	ret = uart_register_driver(&shmac_uart_reg);
+	if (ret)
+		return ret;
+
+	ret = platform_driver_register(&shmac_uart_driver);
+	if (ret)
+		uart_unregister_driver(&shmac_uart_reg);
+
+	return ret;
+}
+
+static void __exit shmac_serial_exit(void)
+{
+	platform_driver_unregister(&shmac_uart_driver);
+	uart_unregister_driver(&shmac_uart_reg);
+}
+
+module_init(shmac_serial_init);
+module_exit(shmac_serial_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:" DRIVER_NAME);
+MODULE_AUTHOR("Joakim Andersson");
+MODULE_DESCRIPTION("SHMAC project serial driver");
