7829922305ca32bc0b5ca40008d4a668cdc91b0f
Håkon Øye Amundsen <hakon.amu@gmail.com>
2014-03-12 13:38:40 +0100

    shmac irq cleanup

--- linux-3.12.13/drivers/irqchip/irq-shmac.c
+++ linux-3.12.13/drivers/irqchip/irq-shmac.c
@@ -45,92 +45,79 @@
 #define IC0_FIRQ_ENABLESET  ((volatile uint32_t*)(INT_CTRL0_BASE+0x28))  
 #define IC0_FIRQ_ENABLECLR  ((volatile uint32_t*)(INT_CTRL0_BASE+0x2c)) 
 
+#define TIMER0_BASE     (TILE_BASE+0x1000)
+#define TIMER0_LOAD      ((volatile uint32_t*)(TIMER0_BASE+0x00))
+#define TIMER0_VALUE     ((volatile uint32_t*)(TIMER0_BASE+0x04))
+#define TIMER0_CTRL      ((volatile uint32_t*)(TIMER0_BASE+0x08))
+#define TIMER0_CLR       ((volatile uint32_t*)(TIMER0_BASE+0x0c))
+#define TIMER_CTRL_ENABLE (1<<7)
+#define TIMER_CTRL_PERIODIC (1<<6)
+#define TIMER_CTRL_SCALE_256 0
+#define TIMER_CTRL_SCALE_16 4
+#define TIMER_CTRL_SCALE_1 8
+#define INT_MASK_HOST 2
 
-/**
- * struct shmac_irq_data - irq data container for the SHMAC IRQ controller
- * @base: memory offset in memory
- * @chip: chip container for this instance
- * @domain: IRQ domain for this instance
- * @valid: mask for valid IRQs on this controller
- * @used_irqs: number of active IRQs on this controller
- */
-
-struct shmac_irq_data {
-	void __iomem *base;
-	struct irq_chip chip;
-	u32 valid;
-	struct irq_domain *domain;
-	u8 used_irqs;
-};
+static void __iomem *base;
+static struct irq_domain *shmac_irq_domain;
 
 /* We only use one of the IRQ-devices on SHMAC */
-static struct shmac_irq_data shmac_irq_device;
 static int shmac_irq_id;
 
-/* also acts as ACK */
+static void shmac_irq_ack(struct irq_data *irqd)
+{
+  printk("SHMACK (empty)\n");
+}
+
 static void shmac_irq_mask(struct irq_data *d)
 {
-	struct shmac_irq_data *s = irq_data_get_irq_chip_data(d);
-	u32 mask = 1 << d->hwirq; 
-
-	writel(mask, s->base + IRQ_ENABLE_CLEAR);
+  unsigned int irq = irqd_to_hwirq(d);
+  unsigned int pos = ffs(irq);
+  printk("SHMIRQ: MASK nr: %d, at pos: %d", irq,pos); 
+	writel((1 << pos), base + IRQ_ENABLE_CLEAR);
 }
 
 
 static void shmac_irq_unmask(struct irq_data *d)
 {
-	struct shmac_irq_data *s = irq_data_get_irq_chip_data(d);
-	u32 mask = 1 << d->hwirq;
-
-	writel(mask, s->base + IRQ_ENABLE_SET);
+  unsigned int irq = irqd_to_hwirq(d);
+  unsigned int pos = ffs(irq);
+  printk("SHMIRQ: UNMASK nr: 0x%x\n", irq); 
+	writel((1 << pos), base + IRQ_ENABLE_SET);
 }
+
+static struct irq_chip shmac_irq_chip= {
+	.name		= "shmac_irq",
+	.irq_ack	= shmac_irq_ack,
+	.irq_mask	= shmac_irq_mask,
+	.irq_unmask	= shmac_irq_unmask,
+};
 
 /* Handle SHMAC irq */
 static asmlinkage void __exception_irq_entry shmac_handle_irq(struct pt_regs *regs)
 {
 	int handled = 0;
 	int irq;
-	u32 status = *SYS_IN_DATA;
-  struct shmac_irq_data *s;
+	u32 status = *IC0_IRQ_STATUS;
+	irq = ffs(status) - 1;
 
-  printk("IRQ, status: 0x%x\n",status);
+  printk("SHMACIRQ: 0x%x",status);
+  printk("\tnr: %d\n\n", irq);
 	writel(0xffffffff, IC0_IRQ_ENABLECLR);
-	s = &shmac_irq_device;
+	writel(0x0, IC0_IRQ_ENABLESET);
+  while(true);
 	while ((status  = readl(IC0_IRQ_STATUS))) {
 		irq = ffs(status) - 1;
-		handle_IRQ(irq_find_mapping(s->domain, irq), regs);
+		//handle_IRQ(irq_find_mapping(shmac_irq_domain, irq), regs);
 		handled = 1;
 	}
-}
-
-
-static void shmac_irq_handle(unsigned int irq, struct irq_desc *desc)
-{
-	struct shmac_irq_data *s = irq_desc_get_handler_data(desc);
-	u32 status = readl(s->base + IRQ_STATUS);
-
-	if (status == 0) {
-		do_bad_IRQ(irq, desc);
-		return;
-	}
-
-	do {
-		irq = ffs(status) - 1;
-		status &= ~(1 << irq);
-		generic_handle_irq(irq_find_mapping(s->domain, irq));
-	} while (status);
 }
 
 static int shmac_irqdomain_map(struct irq_domain *d, unsigned int irq,
 		irq_hw_number_t hwirq)
 {
-	struct shmac_irq_data *s = d->host_data;
   printk("IRQ: Callback irqnr: %d, assigned with:%d\n",(int) hwirq,irq);
-  // Don't bother whith checking validity
-	irq_set_chip_data(irq, s);
-	irq_set_chip_and_handler(irq, &s->chip,
-				handle_level_irq);
-	set_irq_flags(irq, IRQF_VALID | IRQF_PROBE);
+	irq_set_chip_and_handler(irq, &shmac_irq_chip, handle_level_irq);
+	set_irq_flags(irq, IRQF_VALID);
 	return 0;
 }
 
@@ -145,42 +132,22 @@ static struct irq_domain_ops shmac_irqdomain_ops = {
 void __init shmac_irq_init(void __iomem *base, const char *name,
 			  int parent_irq, u32 valid, struct device_node *node)
 {
-	struct shmac_irq_data *s;
 	int i;
-  
-	s = &shmac_irq_device;
-	s->base = base;
-	s->chip.name = name;
-	s->chip.irq_ack = shmac_irq_mask;
-	s->chip.irq_mask = shmac_irq_mask;
-	s->chip.irq_unmask = shmac_irq_unmask;
-	s->valid = valid;
-
-	if (parent_irq != -1) {
-		irq_set_handler_data(parent_irq, s);
-		irq_set_chained_handler(parent_irq, shmac_irq_handle);
-	}
+  int used_irqs = 0;
 
 	/* This will also allocate irq descriptors */
-	s->domain = irq_domain_add_linear(node, fls(valid), &shmac_irqdomain_ops, s);
+	shmac_irq_domain = irq_domain_add_linear(node, 32, &shmac_irqdomain_ops, NULL);
+  shmac_irq_domain->hwirq_max = 10; // TODO necessary ?
 
 	/* This will allocate all valid descriptors in the linear case */
 	for (i = 0; i < fls(valid); i++)
 		if (valid & BIT(i)) {
-			irq_create_mapping(s->domain, i);
-			s->used_irqs++;
+			irq_create_mapping(shmac_irq_domain, i);
+      used_irqs++;
 		}
 
-
-  printk("IRQ: hwirq: %d, irq: %d\n",(int)irq_find_mapping(s->domain, 16),16);
-  printk("IRQ: hwirq: %d, irq: %d\n",(int)irq_find_mapping(s->domain, 17),17);
-	writel(s->valid, base + IRQ_ENABLE_SET);
-  i = * (int*) (base + IRQ_ENABLE_SET);
-  printk("IRQ: Enableset: 0x%x\n", i);
-  printk("IRQ: valid: 0x%x\n", valid);
 	pr_info("SHMAC IRQ chip %d \"%s\" @ %p, %u irqs\n",
-		shmac_irq_id, name, base, s->used_irqs);
-
+		shmac_irq_id, name, base, used_irqs);
 }
 
 
@@ -188,24 +155,23 @@ int __init shmac_of_irq_init(struct device_node *node,
 			    struct device_node *parent)
 {
 	void __iomem *base;
-	u32 valid_mask;
+	u32 valid_mask = 0x1f;
 
   base = of_iomap(node, 0);
   WARN(!base, "unable to map shmac irq registers\n");
 
-	if (of_property_read_u32(node, "valid-mask", &valid_mask))
-		valid_mask = 0;
-  
-  printk("VALID mask: 0x%x\n", valid_mask);
-
   /* Set up IRQ-domain */
 	shmac_irq_init(base, node->name, -1, valid_mask, node);
 
+  // enable timer 0
+  //*TIMER0_LOAD = 60000; // 16 bit register, with implicit left shift 8
+  //*TIMER0_CTRL = TIMER_CTRL_ENABLE | TIMER_CTRL_PERIODIC | TIMER_CTRL_SCALE_1;
+
   /* Clear all pending interrupts */
-	writel(0xffffffff, base + IRQ_ENABLE_CLEAR);
+//	writel(0xffffffff, base + IRQ_ENABLE_CLEAR);
 
   /* Enable the interrupts */
-  writel(valid_mask, base + IRQ_ENABLE_SET);
+  //writel(valid_mask, base + IRQ_ENABLE_SET);
 
 	set_handle_irq(shmac_handle_irq);
 
