406f28552d4988f53bd39cf45d35556deeae3dbb
Joakim Andersson <joakimer@stud.ntnu.no>
2014-04-28 18:10:46 +0200

    updatet scripts and configs

--- scripts/to_shmac_host/ravrun.sh
+++ scripts/to_shmac_host/ravrun.sh
@@ -2,9 +2,16 @@
 
 sudo shmac_reset on
 
+# Write zeroes to the entire memory.
 sudo shmac_program 0x0 zeroes
+
+# Load Bootloader 
 sudo shmac_program 0x0 shboot.bin
+
+# Load Device Tree Blob
 sudo shmac_program 0x4000 dtb.bin
+
+# Load Linux
 sudo shmac_program 0x8000 linux.bin
-sudo shmac_program 0x01000000 ramdisk
+
 sudo shmac_reset off
